da8754b0704c33377aef1041005937ce2365ba3f
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-10-02 14:57:17 -0500

    conf: update author, add secodnary author

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setup(
     description='A DjangoCMS plugin (for TACC Core CMS) to ______.',
     long_description=README,
     url='https://github.com/TACC/__Repo_Name__/',
-    author='TACC ACI WMA',
-    author_email='wma-portals@gmail.com',
+    author='TACC ACI WMA, TACC COA CMD',
+    author_email='wma-portals@tacc.utexas.edu, coa-cmd@tacc.utexas.edu',
     # SEE: https://packaging.python.org/discussions/install-requires-vs-requirements/
     install_requires=[
         'Django>=3.2',
